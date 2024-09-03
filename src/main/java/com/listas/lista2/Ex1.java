431f4f58fa2cf568025e7e6fb8fe94b586190d2a
Felipe-Goncalves-Lima <felipedelima341@gmail.com>
2024-09-03 16:18:43 -0300

    Exercicio 1

---
+++ src/main/java/com/listas/lista2/Ex1.java
@@ -0,0 +1,21 @@
+package com.listas.lista2;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void resolucao2(){
+        //Código de resolução: Exercicio1.resolucao2();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        double numero = sc.nextDouble();
+
+        if(numero%2==0){
+            System.out.println("Número par");
+        }
+        else{
+            System.out.println("Número ímpar");
+        }
+    sc.close();
+    }
+}
