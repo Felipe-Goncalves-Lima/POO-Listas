bce60bea48e16791dca7b6e2e7c779c98b4ba26c
Felipe-Goncalves-Lima <felipedelima341@gmail.com>
2024-09-05 18:11:07 -0300

    Exercicio 3

---
+++ src/main/java/com/listas/lista3/Ex3.java
@@ -0,0 +1,27 @@
+package com.listas.lista3;
+
+import java.util.Scanner;
+
+public class Ex3 {
+    public static void resolucao(){
+
+    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite o primeiro número:");
+    int num1 = scanner.nextInt();
+    System.out.println("Digite o segundo número:");
+    int num2 = scanner.nextInt();
+
+    int inicio = Math.min(num1, num2);
+    int fim = Math.max(num1, num2);
+
+    System.out.println("Números pares entre " + inicio + " e " + fim + ":");
+
+    for (int i = inicio; i <= fim; i++) {
+        if (i % 2 == 0) {
+            System.out.println(i);
+            }
+        }
+    }
+}
